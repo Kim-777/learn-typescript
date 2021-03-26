1a675efcfdae9b2d982f0dd9e418f2720c21b2b2
Kim-777 <iloo3012@naver.com>
2021-03-26 11:29:36 +0900

    typescript study

---
+++ getting-started/index.ts
@@ -0,0 +1,3 @@
+function add2(a: number, b: number) : number {
+    return a + b;
+}
--- why-ts/app.js
+++ why-ts/app.js
@@ -9,13 +9,37 @@ var address = document.querySelector('#address');
 // user data
 var user = {};
 
+/**
+ * @typedef {object} Address
+ * @property {string} street
+ * @property {string} city
+ */
+
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {Address} Address
+ */
+
+/**
+ * 
+ * @returns {Promise<User>}
+ */
+function fetchUser() {
+  return axios.get(url);
+}
+
+
 function startApp() {
-  axios
-    .get(url)
+  fetchUser()
     .then(function (response) {
       console.log(response);
       user = response.data;
       // TODO: 이름, 이메일, 주소 표시하기
+      username.innerHTML = user.name;
+      email.innerText = user.email;
+      address.innerHTML = user.address.city;
     })
     .catch(function (error) {
       console.log(error);
