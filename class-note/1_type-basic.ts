1a4108f499d162956549f834682139041be96c79
Kim-777 <iloo3012@naver.com>
2021-03-27 14:09:05 +0900

    typescript 변수 선언 방식

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,10 @@
+// 문자열
+let str: string = 'hello';
+
+// 숫자
+let num: number = 10;
+
+// Array
+let arr: Array<number> = [1, 2, 3];
+let items: number[] = [1,2,3]
+let heroes: Array<string> = ['Caption', 'Thor', 'Hulk'];
