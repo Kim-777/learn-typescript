f698646615afcfa92c058c52f98f02edf5378812
Kim-777 <iloo3012@naver.com>
2022-02-20 14:40:48 +0900

    Calss - Static Properties

--- with-typescript-handbook/index.ts
+++ with-typescript-handbook/index.ts
@@ -171,6 +171,40 @@ function identity<T>(arg: T): T {
 
 let myIdentity: GenericIdentityFn = identity;
 
+const fullNameMaxLength = 10;
+
+class Employee2 {
+  private _fullName: string;
+
+  get fullName(): string {
+    return this._fullName;
+  }
+
+  set fullName(newName: string) {
+    if (newName && newName.length > fullNameMaxLength) {
+      throw new Error("fullName has a max length of " + fullNameMaxLength);
+    }
+    this._fullName = newName;
+  }
+}
+
+// 전역 프로퍼티 (Static Properties)
+class Grid {
+  static origin = { x: 0, y: 0 };
+  calculateDistanceFromOrigin(point: { x: number; y: number }) {
+    let xDist = point.x - Grid.origin.x;
+    let yDist = point.y - Grid.origin.y;
+    return Math.sqrt(xDist * xDist + yDist * yDist) / this.scale;
+  }
+  constructor(public scale: number) {}
+}
+
+let grid1 = new Grid(1.0); // 1x scale
+let grid2 = new Grid(5.0); // 5x scale
+
+console.log(grid1.calculateDistanceFromOrigin({ x: 10, y: 10 }));
+console.log(grid2.calculateDistanceFromOrigin({ x: 10, y: 10 }));
+
 // Generic Classes
 
 // class GenericNumber<T> {
